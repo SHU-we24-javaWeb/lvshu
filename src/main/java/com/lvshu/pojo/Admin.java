6d13e1b1ce7617edea32287496556147d121322f
perpig <1350575461@qq.com>
2025-02-23 20:03:54 +0800

    管理员实体

---
+++ src/main/java/com/lvshu/pojo/Admin.java
@@ -0,0 +1,56 @@
+package com.lvshu.pojo;
+
+import java.util.Date;
+
+public class Admin {
+    private Integer adminId;
+    private String username;
+    private String password;
+    private Date createdAt;
+
+    // 构造函数
+    public Admin() {}
+
+    // getter和setter方法
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminId=" + adminId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
